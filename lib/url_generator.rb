e70f5bd6027b4da7e7f2f8be4c6d992dd4dd739f
피수영 <vltndud123@naver.com>
2023-10-05 23:18:50 +0900

    modify bug

--- lib/url_generator.rb
+++ lib/url_generator.rb
@@ -2,20 +2,22 @@
 
 require_relative "url_generator/version"
 
-class UrlGenerator
-  def initialize(base_url)
-    @elements = Array.new(base_url)
-  end
+module UrlGenerator
+  class UrlGenerator
+    def initialize(base_url)
+      @elements = Array.new(base_url)
+    end
 
-  def append(element)
-    @elements.append(element)
-  end
+    def append(element)
+      @elements.append(element)
+    end
 
-  def query(key, value)
-    @elements.include?('?') ? @elements.append("&#{key}=#{value}") : @elements.append("?#{key}=#{value}")
-  end
+    def query(key, value)
+      @elements.include?('?') ? @elements.append("&#{key}=#{value}") : @elements.append("?#{key}=#{value}")
+    end
 
-  def build
-    @elements.join
+    def build
+      @elements.join
+    end
   end
 end
